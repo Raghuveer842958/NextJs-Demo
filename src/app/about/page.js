c600327ab4bf0a4f13a123b279974ab148f4bb39
Raghuveer Chauhan <raghuveerchauhan7787@gmail.com>
2025-09-08 07:39:46 +0530

    started learning routing

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <>
+      <h1>Hello This is Raghuveer</h1>
+    </>
+  );
+}
+
---
+++ src/app/user/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function DefaultProfile() {
+  return (
+    <>
+      <h1>Hello This is DefaultProfile page</h1>
+    </>
+  );
+}
