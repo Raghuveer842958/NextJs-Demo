aa79f018d58956f3532baade448c13d3507bd8f3
Raghuveer Chauhan <raghuveerchauhan7787@gmail.com>
2025-10-13 01:22:17 +0530

    fix mongodb connection error

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
       <h1>Hello This is Raghuveer</h1>
       <h1>Hello This is Raghuveer</h1>
       <h1>Hello This is Raghuveer</h1>
-      <h1>Hello This is Raghuveer</h1>
+      {/* <h1>Hello This is Raghuveer</h1> */}
     </>
   );
 }
